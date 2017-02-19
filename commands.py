34873929c84355c99bf51de2ababdf3d8df3f7c0
daniil <bubnov.d.e@gmail.com>
2017-02-19 14:24:23 +0300

    doctests are created for get_operands

--- commands.py
+++ commands.py
@@ -13,6 +13,45 @@ MONEY_COMMAND = Command(name='money', max_operands=3, format='/money сумма 
 
 
 def get_operands(command: Command, message: str) -> list:
+    """Parses message string and retrieves command operands
+
+    >>> get_operands(MONEY_COMMAND, "/money@DnJTodoBot 100; test; 02-15")
+    ['100', 'test', '02-15']
+    >>> get_operands(MONEY_COMMAND, "/money@DnJTodoBot 100; test")
+    ['100', 'test', None]
+    >>> get_operands(MONEY_COMMAND, "/money@DnJTodoBot 100; test; ")
+    ['100', 'test', None]
+    >>> get_operands(MONEY_COMMAND, "/money@DnJTodoBot 100; ; 02-15")
+    ['100', None, '02-15']
+    >>> get_operands(MONEY_COMMAND, "/money@DnJTodoBot 100; ; ")
+    ['100', None, None]
+    >>> get_operands(MONEY_COMMAND, "/money@DnJTodoBot  ; ; ")
+    [None, None, None]
+    >>> get_operands(MONEY_COMMAND, "/money@DnJTodoBot  ; ")
+    [None, None, None]
+    >>> get_operands(MONEY_COMMAND, "/money@DnJTodoBot")
+    [None, None, None]
+    >>> get_operands(MONEY_COMMAND, "")
+    [None, None, None]
+    >>> get_operands(MONEY_COMMAND, "/money@DnJTodoBot asdf;asdf;asdf;asdf")
+    Traceback (most recent call last):
+    ...
+    ValueError: Too many arguments
+    >>> get_operands('WOW', "")
+    Traceback (most recent call last):
+    ...
+    ValueError: Illegal argument types: expected 'Command', actual 'str'
+    >>> get_operands(None, "")
+    Traceback (most recent call last):
+    ...
+    ValueError: Illegal argument types: expected 'Command', actual 'NoneType'
+    """
+
+    if not isinstance(command, Command):
+        raise ValueError("Illegal argument types: "
+                         "expected '{type_expected}', "
+                         "actual '{type_actual}'".format(type_expected=Command.__name__,
+                                                         type_actual=type(command).__name__))
     operands = message.replace('/{}'.format(command.name), '').replace(BOT_NAME, '').strip().split(SEPARATOR)
     result = []
 
